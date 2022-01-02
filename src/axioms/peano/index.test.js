b772d5aeb97b9783e4d90df3dd24a4060b975db3
Gabriel Rufino <contato@gabrielrufino.com>
2022-01-01 23:39:01 -0300

    Initial proof to peano axiom

---
+++ src/axioms/peano/index.test.js
@@ -0,0 +1,30 @@
+const {describe, it, expect} = require('@jest/globals')
+
+const random = require('../../helpers/random')
+
+describe('Peano axioms', () => {
+  it('For every natural number x, x = x. That is, equality is reflexive.', () => {
+    const x = random.natural()
+
+    expect(x === x).toBe(true)
+  })
+
+  it('For all natural numbers x and y, if x = y, then y = x. That is, equality is symmetric.', () => {
+    const x = random.natural()
+    const y = x
+
+    if (x === y) {
+      expect(y === x).toBe(true)
+    }
+  })
+
+  it('For all natural numbers x, y and z, if x = y and y = z, then x = z. That is, equality is transitive.', () => {
+    const x = random.natural()
+    const y = x
+    const z = y
+
+    if (x === y && y === z) {
+      expect(x === z).toBe(true)
+    }
+  });
+})
