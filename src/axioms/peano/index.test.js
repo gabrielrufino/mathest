d612a2bf589b2edc8b43e906576d5ea4b7bb943b
Gabriel Rufino <contato@gabrielrufino.com>
2022-01-03 18:19:14 -0300

    More proofs

--- src/axioms/peano/index.test.js
+++ src/axioms/peano/index.test.js
@@ -1,8 +1,29 @@
 const { describe, it, expect } = require('@jest/globals')
 
 const random = require('../../helpers/random')
+const checkers = require('../../helpers/checkers')
+
+expect.extend({
+  toBeNatural (received) {
+    const pass = checkers.isNatural(received)
+
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be a natural`,
+        pass
+      }
+    } else {
+      return {
+        message: () => `expected ${received} to be a natural`,
+        pass
+      }
+    }
+  }
+})
 
 describe('Peano axioms', () => {
+  const S = x => x + 1
+
   it('For every natural number x, x = x. That is, equality is reflexive.', () => {
     const x = random.natural()
 
@@ -26,5 +47,46 @@ describe('Peano axioms', () => {
     if (x === y && y === z) {
       expect(x === z).toBe(true)
     }
+  })
+
+  it('For all a and b, if b is a natural number and a = b, then a is also a natural number. That is, the natural numbers are closed under equality.', () => {
+    const b = random.natural()
+    const a = b
+
+    expect(a).toBeNatural()
+  })
+
+  it('For every natural number n, S(n) is a natural number. That is, the natural numbers are closed under S.', () => {
+    const n = random.natural()
+
+    expect(S(n)).toBeNatural()
+  })
+
+  it('For all natural numbers m and n, m = n if and only if S(m) = S(n). That is, S is an injection.', () => {
+    const m = random.natural()
+    const n = random.natural()
+
+    expect(m === n).toBe(S(m) === S(n))
+  })
+
+  it('For every natural number n, S(n) = 0 is false. That is, there is no natural number whose successor is 0.', () => {
+    const n = random.natural()
+
+    expect(S(n)).not.toBe(0)
+  })
+
+  describe('Addition', () => {
+    it('a + 0 = a', () => {
+      const a = random.natural()
+
+      expect(a + 0).toBe(a)
+    })
+
+    it('a + S(b) = S(a + b)', () => {
+      const a = random.natural()
+      const b = random.natural()
+
+      expect(a + S(b)).toBe(S(a + b))
+    })
   })
 })
